b2b8b48f17b409a870b98a2f301c6fe22cac3b11
shivam123425 <shivam123425@gmail.com>
2018-05-04 17:14:27 +0530

    Refactor and implemented read and save func

--- notes.js
+++ notes.js
@@ -1,26 +1,35 @@
 const fs=require("fs");
 
+const fetchNote= ()=> {
+    try {
+        return JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch (e) {
+        return [];
+    }
+}
+
+const saveNotes= (notes)=> {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    console.log("Successfully added your note");
+    
+}
+
 let addNote = (title,body)=> {
-    var notes=[];
+    var notes=fetchNote();
+
     var note = {
         title,
         body
     };
-    try {
-        notes = JSON.parse(fs.readFileSync('notes-data.json'));
-    } catch(e) {
-    }
+    
     var duplicateNotes = notes.filter((note)=> note.title===title);
     
     if (duplicateNotes.length !==0) {
         console.log("A note with this title already exist, Please choose different title");   
     } else  {
         notes.push(note);
-        console.log("Your log has been successfully saved");
-        
+        saveNotes(notes);
     }
-    
-    fs.writeFileSync('notes-data.json',JSON.stringify(notes));
 }
 let getNote =(title)=> {
     console.log("Getting the notes", title);
